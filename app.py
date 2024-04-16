11e0534ef92374c013f958228bb491ef32e1b558
nagle-krutika <161570335+nagle-krutika@users.noreply.github.com>
2024-04-16 13:35:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import streamlit as st
+
+file = open("Keys/new_key.txt")
+key = file.read()
+
+client = OpenAI(api_key = key)
+
+st.title("AI-Powered Python Code Fixer")
+
+user_input = st.text_area("Enter python code", height=200)
+
+if st.button("Generate") == True:
+    st.snow()
+    response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": """You are a python code fixer.
+                                                Given a python code should analyze the submitted code and identify potential bugs, errors or areas of improvement.
+                                                First give the suggestion for fixes in numered format with title "Suggestion Fixes" and then corrected code with tile "Corrcted Code"."""},
+                {"role": "user", "content": user_input}
+            ]
+        )
+        
+    st.write(response.choices[0].message.content)
