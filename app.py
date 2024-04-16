5cbbd82bd8cb8f11c1540623d6236eedabcda56b
nagle-krutika <161570335+nagle-krutika@users.noreply.github.com>
2024-04-16 13:40:43 +0530

    app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if st.button("Generate") == True:
             messages=[
                 {"role": "system", "content": """You are a python code fixer.
                                                 Given a python code should analyze the submitted code and identify potential bugs, errors or areas of improvement.
-                                                First give the suggestion for fixes in numered format with title "Suggestion Fixes" and then corrected code with tile "Corrcted Code"."""},
+                                                First give the suggestion for fixes in numbered format with title "Suggestion Fixes" and then corrected code with tile "Corrected Code"."""},
                 {"role": "user", "content": user_input}
             ]
         )
